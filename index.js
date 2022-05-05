08a032c166b270edf88ed05c69ad1318d635a8eb
Filip <filipdopa@yahoo.com>
2022-05-06 01:40:50 +0200

    Implemented image fetching and displaying

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
-import fs from 'node:fs/promises';
-import got from 'got';
+import * as fs from 'node:fs';
+import * as fsAsync from 'node:fs/promises';
+import https from 'node:https';
+import * as cheerio from 'cheerio';
+import fetch from 'node-fetch';
 
-const targetUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
-async function fetchUrls() {
+function fetchImages(urls) {
   try {
-    const response = await got({
-      url: targetUrl,
-    });
-    const images = response.body
-      .replace(/\s/g, '')
-      .match(/<img[^>]*src="[^"]*"[^>]*>/gm)
-      .map((item) => item.replace(/.*src="([^"]*)".*/, '$1'))
-      .slice(0, 10);
-    console.log(images);
-    return images;
-  } catch {
-    console.log("We couldn't get your images");
+    for (let i = 0; i < 10; i++) {
+      https.get(urls[i], (response) => {
+        const filePath =
+          i === 9 ? `./memes/${i + 1}.jpg` : `./memes/0${i + 1}.jpg`;
+        const writeStream = fs.createWriteStream(filePath);
+        response.pipe(writeStream);
+        writeStream.on('finish', () => {
+          writeStream.close();
+        });
+      });
+    }
+    console.log('You can find your dank memes inside of a meme folder!');
+  } catch (err) {
+    console.log(`We couldn't get your images.`);
   }
 }
 
+async function fetchUrls() {
+  const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+  try {
+    const res = await fetch(url);
+    const data = await res.text();
+
+    const parsedHtml = cheerio.load(data, null, false);
+    const imgData = parsedHtml('a');
+    const imgSrc = Object.values(imgData)
+      .map((item) => item.attribs)
+      .slice(6, 16)
+      .map((el) => el.href);
+
+    const imageUrl = imgSrc.map((item) => item.slice(30));
+    return imageUrl;
+  } catch (err) {
+    console.log(err);
+  }
+}
 async function makeDir() {
   try {
-    await fs.mkdir('./memes');
-
+    await fsAsync.mkdir('./memes');
     console.log('We created your directory');
   } catch {
     console.log('You already have directory');
   }
 }
+fetchUrls()
+  .then((imgUrls) => fetchImages(imgUrls))
+  .catch('');
 
-makeDir();
-fetchUrls();
+makeDir().catch('');
